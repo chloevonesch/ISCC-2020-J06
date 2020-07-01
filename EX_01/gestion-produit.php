21d8a050b2b9e0b776633791e9985970ae5271fe
chloevonesch <67325868+chloevonesch@users.noreply.github.com>
2020-07-01 22:56:04 +0200

    J-06

---
+++ EX_01/gestion-produit.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body> 
+<?php
+$nom_produit="T-shirt femme";
+$couleur="Rouge";
+$prix="15.50";
+$disponible="true";
+$quantite="10";
+
+function update-dispo($quantite)
---
+++ EX_01/test-fontions.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body> 
+<?php
+$nom_produit="T-shirt femme";
+$couleur="Rouge";
+$prix="15.50";
+$disponible="true";
+$quantite="10";
+?>
+<a> href="/C:\Users\chloe\Desktop\ISCC-2020\ISCC-2020-J06" </a>;
+
+
+
+
+</body>
+</html>
